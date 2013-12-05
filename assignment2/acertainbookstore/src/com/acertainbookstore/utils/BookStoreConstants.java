6367fb5f895e1d979fc2dc37c914f3776df97280
Henrik <henrik.bendt@gmail.com>
2013-12-05 21:39:04 +0100

    added abort exception message to constants. Used to throw when locks throws exceptions (deadlock)

--- assignment2/acertainbookstore/src/com/acertainbookstore/utils/BookStoreConstants.java
+++ assignment2/acertainbookstore/src/com/acertainbookstore/utils/BookStoreConstants.java
@@ -30,5 +30,8 @@ public final class BookStoreConstants {
 	public static final String NUM_COPIES = "The Number of copies: ";
 	public static final String RATING = "The rating: ";
 	public static final String NULL_INPUT = "null input parameters";
+	
+	//Used for abortions due to deadlocking
+	public static final String ABORT = "Aborted the current operation";
 
 }
