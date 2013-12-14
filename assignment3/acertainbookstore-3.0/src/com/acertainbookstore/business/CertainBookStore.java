2fcb6dfa207ac5b8b0a62692bd0a5eb942adeb5b
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-12-14 13:13:06 +0100

    Added multiple runs to logging and fixed some bugs. Also implemented some pretty printing to the console

--- assignment3/acertainbookstore-3.0/src/com/acertainbookstore/business/CertainBookStore.java
+++ assignment3/acertainbookstore-3.0/src/com/acertainbookstore/business/CertainBookStore.java
@@ -38,6 +38,13 @@ public class CertainBookStore implements BookStore, StockManager {
 
 	}
 
+	/**
+	 * Method added for benchmarking. Should not be available in production ;)
+	 */
+	public synchronized void dropBooks() throws BookStoreException {
+		bookMap = new HashMap<Integer, BookStoreBook>();
+	}
+	
 	public synchronized static CertainBookStore getInstance() {
 		if (singleInstance != null) {
 			return singleInstance;
