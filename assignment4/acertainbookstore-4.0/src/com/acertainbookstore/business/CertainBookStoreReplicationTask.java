cc848ac1acf04cc47c52d17776226a4262f2ba26
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-12-19 12:29:34 +0100

    First part of implementation done, hopefully

--- assignment4/acertainbookstore-4.0/src/com/acertainbookstore/business/CertainBookStoreReplicationTask.java
+++ assignment4/acertainbookstore-4.0/src/com/acertainbookstore/business/CertainBookStoreReplicationTask.java
@@ -1,6 +1,23 @@
 package com.acertainbookstore.business;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.Callable;
+
+import org.eclipse.jetty.client.ContentExchange;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpExchange;
+import org.eclipse.jetty.io.Buffer;
+import org.eclipse.jetty.io.ByteArrayBuffer;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+import com.acertainbookstore.client.BookStoreClientConstants;
+import com.acertainbookstore.server.SlaveBookStoreHTTPMessageHandler;
+import com.acertainbookstore.utils.BookStoreException;
+import com.acertainbookstore.utils.BookStoreMessageTag;
+import com.acertainbookstore.utils.BookStoreResponse;
+import com.acertainbookstore.utils.BookStoreResult;
+import com.acertainbookstore.utils.BookStoreUtility;
 
 /**
  * CertainBookStoreReplicationTask performs replication to a slave server. It
@@ -9,14 +26,63 @@ import java.util.concurrent.Callable;
 public class CertainBookStoreReplicationTask implements
 		Callable<ReplicationResult> {
 
-	public CertainBookStoreReplicationTask() {
-		// TODO Auto-generated constructor stub
+	private String server;
+	private ReplicationRequest request;
+	private HttpClient client;
+	
+	public CertainBookStoreReplicationTask(String server, ReplicationRequest request) {
+		this.server = server;
+		this.request = request;
+		client = new HttpClient();
+		client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
+		client.setMaxConnectionsPerAddress(BookStoreClientConstants.CLIENT_MAX_CONNECTION_ADDRESS); // max
+																									// concurrent
+																									// connections
+																									// to
+																									// every
+																									// address
+		client.setThreadPool(new QueuedThreadPool(
+				BookStoreClientConstants.CLIENT_MAX_THREADSPOOL_THREADS)); // max
+																			// threads
+		client.setTimeout(BookStoreClientConstants.CLIENT_MAX_TIMEOUT_MILLISECS); // seconds
+																					// timeout;
+																					// if
+																					// no
+																					// server
+																					// reply,
+																					// the
+																					// request
+																					// expires
+		try {
+			client.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public ReplicationResult call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		String dataSetXmlString = BookStoreUtility.serializeObjectToXMLString(request.getDataSet());
+		
+		Buffer requestContent = new ByteArrayBuffer(dataSetXmlString);
+
+		ContentExchange exchange = new ContentExchange();
+		String urlString = this.server + "/"
+				+ request.getMessageType();
+		exchange.setMethod("POST");
+		exchange.setURL(urlString);
+		exchange.setRequestContent(requestContent);
+		ReplicationResult retval = new ReplicationResult(this.server, true);
+		
+		try {
+			BookStoreUtility.SendAndRecv(this.client, exchange);
+
+		} catch (BookStoreException e) {
+			retval.setReplicationSuccessful(false);
+		}
+		
+		return retval;
 	}
 
 }
--- assignment4/acertainbookstore-4.0/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
+++ assignment4/acertainbookstore-4.0/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
@@ -1,7 +1,10 @@
 package com.acertainbookstore.business;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import com.acertainbookstore.interfaces.Replicator;
@@ -12,14 +15,24 @@ import com.acertainbookstore.interfaces.Replicator;
  */
 public class CertainBookStoreReplicator implements Replicator {
 
+	ExecutorService exec;
+	
 	public CertainBookStoreReplicator(int maxReplicatorThreads) {
 		// TODO:Implement this constructor
+		exec = Executors.newFixedThreadPool(maxReplicatorThreads);
 	}
 
 	public List<Future<ReplicationResult>> replicate(Set<String> slaveServers,
 			ReplicationRequest request) {
-		// TODO: Implement this method
-		return null;
+		
+		List<Future<ReplicationResult>> results = new ArrayList<Future<ReplicationResult>>();
+		
+		for(String server : slaveServers) {
+			CertainBookStoreReplicationTask task = new CertainBookStoreReplicationTask(server, request);
+			results.add(exec.submit(task));
+		}
+		
+		return results;
 	}
 
 }
